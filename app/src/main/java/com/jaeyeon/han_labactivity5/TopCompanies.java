b7697f17bcc7e25849684d24a414be8ac030b222
JaeJaeJaeJaeJae <2015085918@ust-ics.mygbiz.com>
2019-11-09 23:41:07 +0800

    Initial commit

---
+++ app/src/main/java/com/jaeyeon/han_labactivity5/TopCompanies.java
@@ -0,0 +1,43 @@
+package com.jaeyeon.han_labactivity5;
+
+
+public class TopCompanies {
+
+    private int logo;
+    private String compName,compCountry,compCEO,compIndustry,compDesc;
+
+
+
+    public TopCompanies(int logo, String compName, String compCountry, String compCEO, String compIndustry, String compDesc) {
+        this.logo = logo;
+        this.compName = compName;
+        this.compCountry = compCountry;
+        this.compCEO = compCEO;
+        this.compIndustry = compIndustry;
+        this.compDesc = compDesc;
+    }
+
+    public int getLogo() {
+        return logo;
+    }
+
+    public String getCompName() {
+        return compName;
+    }
+
+    public String getCompCountry() {
+        return compCountry;
+    }
+
+    public String getCompCEO() {
+        return compCEO;
+    }
+
+    public String getCompIndustry() {
+        return compIndustry;
+    }
+
+    public String getCompDesc(){return compDesc;}
+
+
+}
